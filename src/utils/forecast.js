5b289445e86d64277ea52a5c4cb9b94e1b1bde92
ahmad lutfiadi <ahmadlutfiadi@gmail.com>
2019-04-28 20:29:20 +0700

    bug fix 2

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,4 @@
 const request = require('request')
-
-const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-}
-
-module.exports = forecast
-
-const request = require('request')
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/1a24631014982f1ce620226efddf8d2c/' + lat + ',' + long 
     request({ url, json: true }, (error, { body }) => {
