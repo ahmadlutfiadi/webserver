0729cc35b2be467b2ba493d28f182720b51cce06
ahmad lutfiadi <ahmadlutfiadi@gmail.com>
2019-04-28 20:27:30 +0700

    bug fix

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require ('request')
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?types=address&access_token=pk.eyJ1IjoiYWx1dGZpYWRpIiwiYSI6ImNqdWFvdXhldTA0eXg0NHBidWpteThhcG4ifQ.qCxNrzcETIcnR9m-fEpWCg'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYWx1dGZpYWRpIiwiYSI6ImNqdWFvdXhldTA0eXg0NHBidWpteThhcG4ifQ.qCxNrzcETIcnR9m-fEpWCg&limit=1'
     request({
         url,
         json: true
